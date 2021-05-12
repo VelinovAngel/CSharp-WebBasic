ab2a0b4147c7727e14a72ad56c6067eb133ec029
VelinovAngl <kakarum1992@gmail.com>
2021-05-12 22:31:32 +0200

    create mini local server

--- HttpClientDemo/HttpClientDemo/Program.cs
+++ HttpClientDemo/HttpClientDemo/Program.cs
@@ -18,9 +18,34 @@ namespace HttpClientDemo
             // deamon // service 
 
             while (true)
-            {  
+            {
+                const string NewLine = "\r\n";
                 var client = tcpListener.AcceptTcpClient();
-                
+                using (var stream = client.GetStream())
+                {
+
+                    byte[] buffer = new byte[100000];
+                    var lenght = stream.Read(buffer, 0, buffer.Length);
+
+                    var requestString = Encoding.UTF8.GetString(buffer, 0, lenght);
+                    Console.WriteLine(requestString);
+
+
+                    string html = $"<h1> Hello from AngelServer  {DateTime.Now}</h1>";
+
+                    string response = "HTTP/1.1 200 OK"/* 307 Redirect*/ + NewLine +
+                        "Server: AngelServer 2021" + NewLine +
+                        /*"Location: http://www.google.com" + NewLine +*/
+                        "Content-Type: text/html; charset=utf-8" + NewLine +
+                        "Content-Lenght: " + html.Length + NewLine +
+                        NewLine +
+                        html;
+
+                    byte[] responseByte = Encoding.UTF8.GetBytes(response);
+                    stream.Write(responseByte);
+
+                    Console.WriteLine(new string('=', 70));
+                }
             }
         }
 
