d05e7ee7e0258b860d509dc793723c24b0fec9a6
VelinovAngl <kakarum1992@gmail.com>
2021-05-22 22:41:08 +0200

    Http Server and MvcFramework

---
+++ ServerMVC/Apps/MyFirstMvcApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new HttpServer();
+           
+            server.AddRoute("/", HomePage);
+
+            server.AddRoute("/about", About);
+
+            server.AddRoute("/user/login", Login);
+
+            server.Start(80);
+        }
+
+        static HttpResponse HomePage(HttpRequest request)
+        {
+
+        }
+
+        static HttpResponse About(HttpRequest request)
+        {
+
+        }
+
+        static HttpResponse Login(HttpRequest request)
+        {
+
+        }
+    }
+}
---
+++ ServerMVC/SUS.MvcFramework/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SUS.MvcFramework
+{
+    public class Class1
+    {
+    }
+}
