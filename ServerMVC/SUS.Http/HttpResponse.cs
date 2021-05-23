e162a7135fdc93fae7b32b80f029c1f55d6dea39
VelinovAngl <kakarum1992@gmail.com>
2021-05-24 00:12:24 +0200

    Add response

--- ServerMVC/SUS.Http/HttpResponse.cs
+++ ServerMVC/SUS.Http/HttpResponse.cs
@@ -1,6 +1,29 @@
 ﻿namespace SUS.Http
 {
+    using System;
+    using System.Collections.Generic;
+
+    using SUS.Http.Enums;
+
     public class HttpResponse
     {
+        public HttpResponse(string contentType, byte[] body, HttpStatusCode statusCode = HttpStatusCode.Ok)
+        {
+            this.StatusCode = statusCode;
+            this.Body = body;
+            this.Header = new List<Header>
+            {
+                {new Header ("Content-Type", contentType) },
+                {new Header("Contente-Length", body.Length.ToString()) }
+
+            };
+
+
+        }
+
+        public byte[] Body { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public ICollection<Header> Header { get; set; }
+
     }
 }
