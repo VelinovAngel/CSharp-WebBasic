3b40182a2ab216965b2fe552b9d6bd67af97c4ec
VelinovAngl <kakarum1992@gmail.com>
2021-05-29 17:47:58 +0200

    Generate C# code from template

--- ServerMVC/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
+++ ServerMVC/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
@@ -6,6 +6,48 @@
     {
         public string GetHtml(string templateCode, object viewModel)
         {
+            string csharpCode = GenerateCSharpFromTemplate(templateCode);
+            IView executableObject = GenerateExecutableCode(csharpCode);
+            string html = executableObject.ExecuteTemplate(viewModel);
+            return html;
+        }
+
+        private string GenerateCSharpFromTemplate(string templateCode)
+        {
+            string methodBody = GetMethodBody(templateCode);
+            string csharpCode = @"
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using SUS.MvcFramework.ViewEngine;
+
+namespace ViewNamespace
+{
+    public class ViewClass : IView
+    {
+        public string ExecuteTemplate(object viewModel)
+        {
+            var html = new StringBuilder();
+
+            "+ methodBody + @"          
+
+            return html.ToString();
+        }
+    }
+}
+";
+
+            return csharpCode;
+        }
+
+        private string GetMethodBody(string templateCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IView GenerateExecutableCode(object csharpCode)
+        {
             throw new NotImplementedException();
         }
     }
