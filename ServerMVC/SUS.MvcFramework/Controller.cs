3111853d00023d0659d17bd06e216de182d69b1f
VelinovAngl <kakarum1992@gmail.com>
2021-06-02 12:03:38 +0200

    add HttpResponse error when i have error

--- ServerMVC/SUS.MvcFramework/Controller.cs
+++ ServerMVC/SUS.MvcFramework/Controller.cs
@@ -17,18 +17,34 @@
         }
         public HttpResponse View(object viewModel = null, [CallerMemberName] string viewPath = null)
         {
-            var layout = System.IO.File.ReadAllText("Views/Shared/_Layout.cshtml");
-            layout = layout.Replace("@RenderBody()", "_____VIEW_GOES_HERE_____");
-            layout = this.viewEngine.GetHtml(layout, viewModel);
-
             var viewContent = System.IO.File.ReadAllText("Views/" + this.GetType().Name.Replace("Controller", string.Empty) + "/" + viewPath + ".cshtml");
             viewContent = this.viewEngine.GetHtml(viewContent, viewModel);
 
-            var responseHtml = layout.Replace("_____VIEW_GOES_HERE_____", viewContent);
+            var responseHtml = this.PutViewLayout(viewContent, viewModel);
+
             var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
             var response = new HttpResponse("text/html", responseBodyBytes);
-
             return response;
+        }
+
+        public HttpResponse Error(string errorText)
+        {
+            var viewContent = $"<div class=\"alert alert-danger\" role=\"alert\">{errorText}</div>";
+            var responseHtml = this.PutViewLayout(viewContent);
+
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes, HttpStatusCode.ServerError);
+            return response;
+        }
+
+        private string PutViewLayout(string viewContent, object viewModel = null)
+        {
+            var layout = System.IO.File.ReadAllText("Views/Shared/_Layout.cshtml");
+            layout = layout.Replace("@RenderBody()", "_____VIEW_GOES_HERE_____");
+            layout = this.viewEngine.GetHtml(layout, viewModel);
+            var responseHtml = layout.Replace("_____VIEW_GOES_HERE_____", viewContent);
+
+            return responseHtml;
         }
 
         public HttpRequest Request { get; set; }
