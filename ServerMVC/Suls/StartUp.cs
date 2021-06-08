bac2bcadfc61962492944714ec5be85a12bae6c3
VelinovAngl <kakarum1992@gmail.com>
2021-06-08 21:54:47 +0200

    Create new project skeleton

--- ServerMVC/Suls/StartUp.cs
+++ ServerMVC/Suls/StartUp.cs
@@ -1,6 +1,22 @@
 ﻿namespace Suls
 {
-    public class StartUp 
+    using SUS.Http;
+    using SUS.MvcFramework.Contracts;
+
+    using System.Collections.Generic;
+
+    public class StartUp : IMvcApplication
     {
+        public void Configure(List<Route> routeTable)
+        {
+            //db.migrate
+            throw new System.NotImplementedException();
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // Dependency container
+            throw new System.NotImplementedException();
+        }
     }
 }
