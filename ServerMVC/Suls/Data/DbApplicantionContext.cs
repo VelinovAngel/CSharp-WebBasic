fc376b40dff6ffc144a3730aa541a7e0edc7dc6a
VelinovAngel <kakarum1992@gmail.com>
2021-06-09 21:42:44 +0200

    Add DbApplicantionContext

---
+++ ServerMVC/Suls/Data/DbApplicantionContext.cs
@@ -0,0 +1,27 @@
+﻿namespace Suls.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class DbApplicantionContext : DbContext
+    {
+        private const string SqlServer = @"Server=.\SQLEXPRESS;Database=Suls;Integrated Security=TRUE";
+
+        protected DbApplicantionContext()
+        {
+        }
+        public DbApplicantionContext(DbContextOptions dbContextOptions)
+            :base(dbContextOptions)
+        { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(SqlServer);
+            }
+        }
+    }
+
+
+}
