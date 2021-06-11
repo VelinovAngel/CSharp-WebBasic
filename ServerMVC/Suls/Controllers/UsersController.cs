dcae33f3eb9034671ceee6f2e5b78ac9b1140d65
VelinovAngel <kakarum1992@gmail.com>
2021-06-11 21:31:51 +0200

    Create UserController

--- ServerMVC/Suls/Controllers/UsersController.cs
+++ ServerMVC/Suls/Controllers/UsersController.cs
@@ -1,11 +1,20 @@
 ﻿namespace Suls.Controllers
 {
+    using System.ComponentModel.DataAnnotations;
+
     using SUS.Http;
+    using Suls.Services;
     using SUS.MvcFramework;
     using Suls.ViewModels.Users;
- 
+
     public class UsersController : Controller
     {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
         public HttpResponse Login()
         {
             return this.View();
@@ -14,6 +23,13 @@
         [HttpPost("Users/Login")]
         public HttpResponse Login(string username , string password)
         {
+            var userId = this.userService.GetUserId(username, password);
+            if (userId == null)
+            {
+                return this.Error("Invalid username or password!");
+            }
+            this.SignIn(userId);
+
             return this.Redirect("/");
         }
 
@@ -24,7 +40,33 @@
 
         public HttpResponse Register(UserViewModel model)
         {
-            //TODO all logical
+            if (string.IsNullOrEmpty(model.Username) || model.Username.Length < 5 || model.Username.Length > 20)
+            {
+                this.Error("The username should be between 5 and 20 characters!");
+            }
+            if (string.IsNullOrEmpty(model.Email) || !new EmailAddressAttribute().IsValid(model.Email))
+            {
+                this.Error("The email address should be valid!");
+            }
+            if (string.IsNullOrEmpty(model.Password) || model.Password.Length > 6 || model.Password.Length < 20)
+            {
+                this.Error("Invalid password");
+            }
+            if (model.Password != model.ConfirmPassword)
+            {
+                this.Error("Invalid password!");
+            }
+            if (userService.IsEmailAvailable(model.Email))
+            {
+                this.Error("Email already taken");
+            }
+            if (userService.IsUserAvailable(model.Username))
+            {
+                this.Error("Username already taken");
+            }
+
+            userService.CreateUser(model.Username, model.Email, model.Password);
+
             return this.Redirect("/Users/Login");
         }
     }
--- ServerMVC/Suls/Services/UserService.cs
+++ ServerMVC/Suls/Services/UserService.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Suls.Services
+﻿namespace Suls.Services
 {
-    class UserService
+    using System.Linq;
+    using System.Text;
+    using System.Security.Cryptography;
+
+    using Suls.Data;
+
+    public class UserService : IUserService
     {
+        private readonly DbApplicationContext db;
+
+        public UserService(DbApplicationContext db)
+        {
+            this.db = db;
+        }
+
+        public void CreateUser(string username, string email, string password)
+        {
+            var user = new User
+            {
+                Email = email,
+                Username = username,
+                Password = ComputeHash(password),
+
+            };
+
+            db.Users.Add(user);
+            db.SaveChanges();
+        }
+
+        public string GetUserId(string username, string password)
+        {
+            var passwordHash = ComputeHash(password);
+            var user = db.Users.FirstOrDefault(x => x.Username == username && x.Password == passwordHash);
+            return user?.Id;
+        }
+
+        public bool IsEmailAvailable(string email)
+                => !this.db.Users.Any(x => x.Email == email);
+
+        public bool IsUserAvailable(string username)
+                => !this.db.Users.Any(x => x.Username == username);
+
+        private static string ComputeHash(string input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input);
+            using (var hash = SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+                // Convert to text
+                // StringBuilder Capacity is 128, because 512 bits / 8 bits in byte * 2 symbols for byte 
+                var hashedInputStringBuilder = new StringBuilder(128);
+                foreach (var b in hashedInputBytes)
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                return hashedInputStringBuilder.ToString();
+            }
+        }
     }
 }
