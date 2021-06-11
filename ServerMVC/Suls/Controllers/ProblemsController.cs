983bbfe8ab27b0ec97ef0e878a49a146d4198af1
VelinovAngel <kakarum1992@gmail.com>
2021-06-11 23:07:50 +0200

    Create ProblemController

--- ServerMVC/Suls/Controllers/ProblemsController.cs
+++ ServerMVC/Suls/Controllers/ProblemsController.cs
@@ -21,7 +21,16 @@
         [HttpPost]
         public HttpResponse Create(string name, int points)
         {
-          
+            if (name.Length < 5 || name.Length > 20 || string.IsNullOrEmpty(name))
+            {
+                this.Error("Invalid problem name!");
+            }
+            if (points < 50 || points > 300)
+            {
+                this.Error("Invalid points!");
+            }
+            problemService.CreateProblem(name, points);
+            return this.View();
         }
     }
 }
--- ServerMVC/Suls/Services/ProblemService.cs
+++ ServerMVC/Suls/Services/ProblemService.cs
@@ -1,6 +1,31 @@
-﻿namespace Suls.Services
+﻿using Suls.Data;
+
+namespace Suls.Services
 {
     public class ProblemService : IProblemService
     {
+        private readonly DbApplicationContext db;
+
+        public ProblemService(DbApplicationContext db)
+        {
+            this.db = db;
+        }
+
+        public void CreateProblem(string name, int points)
+        {
+            var problem = new Problem
+            {
+                Name = name,
+                Points = points,
+            };
+
+            this.db.Problems.Add(problem);
+            this.db.SaveChanges();
+        }
+
+        public string GetProblemId(string name)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
