fe07faa66173eb9c89eccf72cfb9afa123b9b85e
VelinovAngel <kakarum1992@gmail.com>
2021-06-13 16:44:54 +0200

    Change project directory

---
+++ ServerMVC/Apps/SharedTrip/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using SUS.Http;
+using SUS.MvcFramework.Contracts;
+using System.Collections.Generic;
+
+namespace SharedTrip
+{
+    internal class StartUp : IMvcApplication
+    {
+        public void Configure(List<Route> routeTable)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
