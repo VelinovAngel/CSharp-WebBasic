f804559d07718a896ae373774ebff038392a64b4
VelinovAngel <kakarum1992@gmail.com>
2021-06-16 22:47:05 +0200

    Add attributes

--- CSharp-Web-Server/MyWebServer.App/Data/Models/Repository.cs
+++ CSharp-Web-Server/MyWebServer.App/Data/Models/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyWebServer.App.Data.Models
 {
@@ -12,10 +13,13 @@ namespace MyWebServer.App.Data.Models
         }
         public string Id { get; set; }
 
+        [Required]
+        [MaxLength(10)]
         public string Name { get; set; }
 
         public DateTime CreatedOn { get; set; }
 
+        [Required]
         public bool IsPublic { get; set; }
 
         public string OwnerId { get; set; }
