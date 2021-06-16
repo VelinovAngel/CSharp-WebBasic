56d2ee17f85c53dc18a26fa885bdf155f18e5f19
VelinovAngel <kakarum1992@gmail.com>
2021-06-16 22:22:20 +0200

    Introduce DbContex

---
+++ CSharp-Web-Server/MyWebServer.App/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿namespace MyWebServer.App.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+
+    public class ApplicationDbContext : DbContext
+    {
+        private const string SqlServerAddress = @"Server=.\SQLEXPRESS;Database=Git;Integrated Security=TRUE";
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions dbContextOptions)
+            :base(dbContextOptions)
+        {
+        }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(SqlServerAddress);
+            }
+        }
+    }
+}
