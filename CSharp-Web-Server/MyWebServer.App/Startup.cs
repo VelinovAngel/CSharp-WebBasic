8acaadb3a7d3a61886c06955e258a7a9750e66e6
VelinovAngel <kakarum1992@gmail.com>
2021-06-16 22:25:26 +0200

    Introduce DbContex

--- CSharp-Web-Server/MyWebServer.App/Startup.cs
+++ CSharp-Web-Server/MyWebServer.App/Startup.cs
@@ -1,20 +1,20 @@
-﻿using System.Threading.Tasks;
-using MyWebServer.App.Controllers;
-using MyWebServer.Controllers;
-using MyWebServer.Results.Views;
-
-namespace MyWebServer.App
+﻿namespace MyWebServer.App
 {
+    using System.Threading.Tasks;
+    using MyWebServer.App.Data;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
     public class Startup
     {
         public static async Task Main()
             => await HttpServer
                 .WithRoutes(routes => routes
                     .MapStaticFiles()
-                    .MapControllers()
-                .MapGet<UsersController>("/Users/Login", c=>c.Login()))
+                    .MapControllers())
                 .WithServices(services => services
-                    .Add<IViewEngine, CompilationViewEngine>())
+                    .Add<IViewEngine, CompilationViewEngine>()
+                .Add<ApplicationDbContext>())
                 .Start();
     }
 }
