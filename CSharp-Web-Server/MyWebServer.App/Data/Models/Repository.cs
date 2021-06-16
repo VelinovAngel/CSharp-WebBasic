a2fada67fdec06e2661a126ef8a07c1d891ff4d0
VelinovAngel <kakarum1992@gmail.com>
2021-06-16 22:45:12 +0200

    Add entity Repository

--- CSharp-Web-Server/MyWebServer.App/Data/Models/Repository.cs
+++ CSharp-Web-Server/MyWebServer.App/Data/Models/Repository.cs
@@ -1,6 +1,27 @@
-﻿namespace MyWebServer.App.Data.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyWebServer.App.Data.Models
 {
     public class Repository
     {
+        public Repository()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Commits = new HashSet<Commit>();
+        }
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public string OwnerId { get; set; }
+
+        public User Owner { get; set; }
+
+        public ICollection<Commit> Commits { get; set; }
     }
 }
