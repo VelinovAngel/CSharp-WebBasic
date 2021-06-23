3782aa234b8d878c77dfe34a4694b29cfef85e2e
VelinovAngel <kakarum1992@gmail.com>
2021-06-23 21:23:49 +0200

    Add new exam

---
+++ CSharp-Web-Server/Apps/Andrey/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace Andrey
+{
+    using MyWebServer;
+    using System.Threading.Tasks;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
+    public class StartUp
+    {
+        static async Task Main()
+            => await HttpServer
+                .WithRoutes(route => route
+                    .MapStaticFiles()
+                    .MapControllers())
+                        .WithServices(service => service
+                            .Add<IViewEngine, CompilationViewEngine>())
+                .Start();
+    }
+}
