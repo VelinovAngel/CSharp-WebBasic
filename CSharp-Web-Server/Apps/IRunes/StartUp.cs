9118eff163b53f1a3951cd510703c22227c1c9f1
VelinovAngel <kakarum1992@gmail.com>
2021-06-24 22:33:37 +0200

    Start new exam

---
+++ CSharp-Web-Server/Apps/IRunes/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace IRunes
+{
+    using MyWebServer;
+    using System.Threading.Tasks;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
+    public class StartUp
+    {
+        static async Task Main()
+            => await HttpServer
+                .WithRoutes(routes => routes
+                .MapStaticFiles()
+                .MapControllers())
+            .WithServices(services => services
+                .Add<IViewEngine, CompilationViewEngine>())
+            .Start();
+    }
+}
