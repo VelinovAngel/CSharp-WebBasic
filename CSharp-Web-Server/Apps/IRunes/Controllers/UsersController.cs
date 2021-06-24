0817fc8ef683a5c6a9b05baec7820d4e456e1544
VelinovAngel <kakarum1992@gmail.com>
2021-06-24 23:53:43 +0200

    Introduce validations

--- CSharp-Web-Server/Apps/IRunes/Controllers/UsersController.cs
+++ CSharp-Web-Server/Apps/IRunes/Controllers/UsersController.cs
@@ -1,17 +1,28 @@
 ﻿namespace IRunes.Controllers
 {
+    using IRunes.Services;
     using IRunes.ViewModels;
     using MyWebServer.Controllers;
     using MyWebServer.Http;
 
     public class UsersController : Controller
     {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
         public HttpResponse Register() => this.View();
 
         [HttpPost]
         public HttpResponse Register(RegisterUserFormModel model)
         {
+            if (model.Password != model.ConfirmPassword)
+            {
 
+            }
             return this.Redirect("/");
         }
     }
