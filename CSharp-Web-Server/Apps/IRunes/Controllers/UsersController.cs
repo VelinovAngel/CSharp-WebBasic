28c7b29687172e7304c1d71e3e9a16cf541d755f
VelinovAngel <kakarum1992@gmail.com>
2021-06-24 23:57:15 +0200

    Introduce validations

--- CSharp-Web-Server/Apps/IRunes/Controllers/UsersController.cs
+++ CSharp-Web-Server/Apps/IRunes/Controllers/UsersController.cs
@@ -23,6 +23,19 @@
             {
                 return this.Redirect("/Users/Register");
             }
+            if (string.IsNullOrWhiteSpace(model.Username) || model.Username.Length < 4 || model.Username.Length > 20)
+            {
+                return this.Redirect("/Users/Register");
+            }
+            if (string.IsNullOrWhiteSpace(model.Email))
+            {
+                return this.Redirect("/Users/Register");
+            }
+            if (model.Password.Length < 6 || model.Password.Length > 20)
+            {
+                return this.Redirect("/Users/Register");
+            }
+            this.userService.Register(model);
             return this.Redirect("/");
         }
     }
